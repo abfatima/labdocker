151fe143340ad48590b3b6e317de3bab52500287
fatima benarab <fatimabenarab9@gmail.com>
2024-02-07 15:29:31 +0100

    first commit

---
+++ src/main/java/com/example/consumingwebservice/LabdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdockerApplication.class, args);
+	}
+
+}
